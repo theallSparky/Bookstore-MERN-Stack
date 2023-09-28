a63a7d963302dc785a95df94c37cc06c41d3d8c3
theallSparky <harrisonsparks.1999@gmail.com>
2023-09-28 09:39:40 -0500

    Added a better data structure for our books in the app.get('/books') page. Bow includes total number of books in the list.

--- backend/index.js
+++ backend/index.js
@@ -38,6 +38,19 @@ app.post('/books', async (request, response) => {
     }
 })
 
+app.get('/books', async (request, response) => {
+    try {
+        const books = await Book.find({})
+
+        return response.status(200).json({
+            count: books.length,
+            data: books
+        })
+    } catch (error) {
+        console.log(error.message)
+        response.status(500).send({ message: error.message })
+    }
+})
 
 mongoose
     .connect(mongoDBURL)
