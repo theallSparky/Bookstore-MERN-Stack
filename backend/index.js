d78c0433f4535d1be980b9f1f24f9a2b94aa9811
theallSparky <harrisonsparks.1999@gmail.com>
2023-09-28 10:48:08 -0500

    Added a put route to update book info

--- backend/index.js
+++ backend/index.js
@@ -65,6 +65,32 @@ app.get('/books/:id', async (request, response) => {
     }
 })
 
+app.put('/books/:id', async (request, response) => {
+    try {
+        if (
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ) {
+            return response.status(400).send({
+                message: 'Send all required fields: title, author, publishYear',
+            })
+        }
+
+        const { id } = request.params
+
+        const result = await Book.findByIdAndUpdate(id, request.body)
+
+        if (!result) {
+            return response.status(404).json({ message: 'Book not found!' })
+        } // For some reason, error 404 is not showing in postman
+        return response.status(200).send({ message: 'Book updated successfully!' })
+    } catch (error) {
+        console.log(error.message)
+        response.status(500).send({ message: error.message })
+    }
+})               
+
 mongoose
     .connect(mongoDBURL)
     .then(()=> {
