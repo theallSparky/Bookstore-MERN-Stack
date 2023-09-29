0e7290ee937727e65016f20530e880352a0812a3
theallSparky <harrisonsparks.1999@gmail.com>
2023-09-28 22:40:32 -0500

    Added cors

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,20 @@
 import express from 'express'
 import { PORT, mongoDBURL } from './config.js'
 import mongoose from 'mongoose'
-import { Book } from './models/bookModel.js'
 import booksRoute from './routes/booksRoute.js'
+import cors from 'cors'
 
 const app = express()
 
 app.use(express.json())
+
+app.use(
+    cors({
+        origin: 'http://localhost:5555',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type']
+    })
+)
 
 app.get('/', (request,response) => {
     console.log(request)
