481481175b9915491d7b0b4660f2588689e5680a
theallSparky <harrisonsparks.1999@gmail.com>
2023-10-02 00:45:22 -0500

    Finished work on a basic functioning page for deleting a book. The book now deletes from the database

--- frontend/src/pages/DeleteBook.jsx
+++ frontend/src/pages/DeleteBook.jsx
@@ -25,6 +25,17 @@ const DeleteBook = () => {
   return (
     <div className='p-4'>
       <BackButton />
+      <h1 className='text-3xl my-4'>Delete Book</h1>
+      {loading ? <Spinner /> : ''}
+      <div className='flex xlex-col ittems-center border-2 border-sky-400 rounded-xl w-[600px] p-8 mx-auto'>
+        <h3 className='text-2xl'>Are You Sure You Want to Delete This Book?</h3>
+
+        <button
+          className='p-4 bg-red-600 text-white m-8 w-full'
+          onClick={handleDeleteBook}>
+            Yes, Delete It!
+          </button>
+      </div>
     </div>
   )
 }
