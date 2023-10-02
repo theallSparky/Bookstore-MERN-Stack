075d390e6abf9b00c2f0a21dc7490fdad11358ca
theallSparky <harrisonsparks.1999@gmail.com>
2023-10-02 00:37:34 -0500

    Added a functional back button to the DeleteBook page

--- frontend/src/pages/DeleteBook.jsx
+++ frontend/src/pages/DeleteBook.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import BackButton from '../components/BackButton'
+import Spinner from '../components/Spinner'
+import axios from 'axios'
+import { useNavigate, useParams } from 'react-router-dom'
 
 const DeleteBook = () => {
+  const [loading, setLoading] = useState(false)
+  const navigate = useNavigate()
+  const { id } = useParams()
+  const handleDeleteBook = () => {
+    setLoading(true)
+    axios
+      .delete(`http://localhost:5555/books/${id}`)
+      .then(() => {
+        setLoading(false)
+        navigate('/')
+        .catch((error) => {
+          setLoading(false)
+          alert('An error occurred! Please check the console!')
+          console.log(error)
+        })
+      })
+  }
   return (
-    <div>DeleteBook</div>
+    <div className='p-4'>
+      <BackButton />
+    </div>
   )
 }
 
