c693095b2f7e6a3062c1eea073b2e90a15afa973
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-12-19 09:43:22 -0600

    Cleaned up backend/index.js. Deleted unnecessary commented code. Fixed .env

--- backend/index.js
+++ backend/index.js
@@ -1,38 +1,32 @@
-import express from 'express'
-import { PORT, mongoDBURL } from './config.js'
-import mongoose from 'mongoose'
-import booksRoute from './routes/booksRoute.js'
-import cors from 'cors'
+import express from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+import booksRoute from "./routes/booksRoute.js";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.use(cors())
+app.use(cors());
 
-// app.use(
-//     cors({
-//         origin: 'http://localhost:5555',
-//         methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//         allowedHeaders: ['Content-Type']
-//     })
-// )
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(234).send("Welcome to the Bookstore!");
+});
 
-app.get('/', (request,response) => {
-    console.log(request)
-    return response.status(234).send('Welcome to the Bookstore!')
-})
-
-app.use('/books', booksRoute)
+app.use("/books", booksRoute);
 
 mongoose
-    .connect(mongoDBURL)
-    .then(()=> {
-        console.log(`App connected to database!`)
-        app.listen(PORT, ()=> {
-            console.log(`App is litening to port: ${PORT}`)
-        })
-    })
-    .catch((error)=> {
-        console.log(error)
-    })
+  .connect(process.env.mongoDBURL)
+  .then(() => {
+    console.log(`App connected to database!`);
+    app.listen(PORT, () => {
+      console.log(`App is litening to port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
